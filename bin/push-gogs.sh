fe77da431fe0ab51f9511bed70c29e365281066e
caracan <adowns@redhat.com>
2015-07-23 14:54:44 +0100

    Updated the remote origin to use the Service IP and http

--- bin/push-gogs.sh
+++ bin/push-gogs.sh
@@ -10,9 +10,10 @@ callback() {
 
   pushd $1/$2 &>/dev/null
   git remote rm origin
-  git remote add origin ssh://git@localhost:2222/$INTEGRATION/$(basename $(realpath .))
+  git remote add origin http://administrator:$DEMOPW@$SVCIP/$INTEGRATION/$(basename $(realpath .))
   GIT_SSH=../../bin/issh git push -u origin master
   popd &>/dev/null
 }
 
+SVCIP=$(oc get services gogs -o template --template='{{.spec.portalIP}}')
 all_repos "$MONSTER_REPOS" monster callback
